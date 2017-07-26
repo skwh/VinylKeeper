e07699a1d497428d7d0477f52062dd3d14bed5d2
Evan Derby <somekidwithhtml@gmail.com>
2017-07-25 21:43:11 -0600

    Add todo items for python code

--- vk_server.py
+++ vk_server.py
@@ -9,12 +9,15 @@ class VKServer:
     def __init__(self, port_number, database_path):
         self.port_number = port_number
         self.database = database_path
+        # TODO: re-write handler for Angular & database api usage
         self.request_handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        # TODO: args & handling for server address
         self.http_server = SocketServer.TCPServer(("0.0.0.0", port_number), self.request_handler)
         self.database_handler = VKDatabase(self.database)
         self._register_sigint()
 
     def _register_sigint(self):
+        # TODO: sigint graceful shutdown
         signal.signal(signal.SIGINT, self.stop)
 
     def _print_address(self):
